83f14ca16522a39b411b5fb5edeb3854d050a836
Pierre78310 <pierre78prm@gmail.com>
2017-12-06 21:25:48 +0100

    mise a jour fichiers

---
+++ tests/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/footer.css">
+
+    <footer>
+      <a href="" class="">Notre facebook</a></br> <!-- A compléter (c'est Domisep qui décidera de rajouter son lien db s'il en a un)-->
+      <a href="mailto:machin@domisep.fr">Nous contacter par mail !</a></br> <!--A compléter par Domisep-->
+      <p>Notre numéro :</p> <!--A compléter par Domisep--></br>
+      <!--Rajouter d'autres réseaux sociaux si Domisep en a-->
+      <a href="" class="">Conditions générales d'utilisation</a></br>
+      <a href="" class="">Qui sommes nous ?</a></br>
+      <a href="" class="">Manuel d'utilisation</a></br>
+      <label for="langues">Langue :</label>
+      <select name="langues" id="langues">
+        <option value="français">Français</option>
+        <option value="anglais">English</option>
+    </footer>
---
+++ tests/mes_capteurs_et_cameras.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/mes_capteurs_et_cameras.css">
+    <title></title>
+  </head>
+  <body>
+    <?php include('new_header.php');?>
+    <!--AJOUTER FIL ARIANE-->
+    <div id="choix">
+      <div id="activer_capteur">
+        <a href="#">Activer des capteurs</a>
+      </div>
+      <div id="desactiver_capteur">
+        <a href="#">Désactiver des capteurs</a>
+      </div>
+      <div id="mode_automatique">
+        <a href="#">Mode automatique</a>
+      </div>
+      <div id="afficher_donnees">
+        <a href="#">Données remontées par les capteurs</a>
+      </div>
+    </div>
+
+
+    <?php include('footer.php');?>
+
+  </body>
+</html>
