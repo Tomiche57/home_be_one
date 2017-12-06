83f9e97700fb6ac69f1be163dd60a0e795a57091
antoine_com <antoine.comaita@hotmail.fr>
2017-12-06 23:59:39 +0100

    effacer modif

--- connexion_gustave_cible.php
+++ connexion_gustave_cible.php
@@ -4,9 +4,9 @@ $_SESSION['pseudo']=$_POST['pseudo'];
 
 <!DOCTYPE html>
 
-    
+
     <?php
-    
+
     // Vérification des identifiants
     try
     {
@@ -16,7 +16,7 @@ $_SESSION['pseudo']=$_POST['pseudo'];
     {
         die('Erreur : '.$e->getMessage());
     }
-    
+
      $delai=2;
      $url='http://localhost/tests/home_be_one/connexion_gustave.php';
     /* $prenom=$_POST['prenom'] ; */
@@ -24,13 +24,13 @@ $_SESSION['pseudo']=$_POST['pseudo'];
     $requser = $bdd->prepare('SELECT id FROM utilisateur WHERE pseudo = :pseudo AND mdp= :mdp ');
     $requser->execute(array('pseudo' => $_POST['pseudo'],
         'mdp' => $_POST['mdp']));
-    
+
     $resultat = $requser->fetch();  ?>
- 
+
     <?php
     if ($resultat==0)
-    {  
-        
+    {
+
         echo 'erreur de saisie';
         header("Refresh: $delai;url=$url");
 
@@ -38,14 +38,14 @@ $_SESSION['pseudo']=$_POST['pseudo'];
     }
     else
 
-    {   
+    {
         $_SESSION['pseudo']=$_POST['pseudo'];
         $delai=1;
         $url='http://localhost/tests/home_be_one/accueil_connecte_okok.php';
-        echo 'Veuillez patienter';
+        echo 'Veuillez patienter svp';
         header("Refresh: $delai;url=$url");
         exit();
-    }  ?>  
-    
+    }  ?>
+
 
 </html>
