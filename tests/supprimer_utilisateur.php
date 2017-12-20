86406fa5436972fa9003571429653a2b2d72b0bd
antoine19 <antoine.comaita@hotmail.fr>
2017-12-20 01:21:31 +0100

    page pour supprimer utilisateur -->veuillez tester

--- tests/supprimer_utilisateur.php
+++ tests/supprimer_utilisateur.php
@@ -5,14 +5,17 @@ session_start();
 
 <html>
   <head>
+
+
     <meta charset="utf-8" >
+    <link rel="stylesheet" href="css/supprimer_utilisateur.css">
     <title>Supprimer utilisateur</title>
   </head>
   <body>
 
 
     <h1> Etes vous sur de vouloir supprimer votre compte ? </h1>
-
+    <div id="form">
       <form action="" method="post">
         <br />
                <input type="checkbox" name="oui" id="oui" /> <label for="oui">OUI</label><br />
@@ -20,38 +23,38 @@ session_start();
 
 
         <input type="submit" name="Valider" value="envoyer">
+    </div>    
       </form>
-      <?php if (isset($erreur)):
+      <?php if (isset($erreur))
         echo $erreur;
        ?>
   </body>
 </html>
-
-
-
 <?php
 
-  if (isset($_POST['Valider']) AND !empty($_POST['Valider']=='envoyer')):
+  if (isset($_POST['Valider']) AND !empty($_POST['Valider']=='envoyer'))
   {
-    if (isset($_POST['oui']) and !empty($_POST['oui']))
+    if (isset($_POST['oui']) and !empty($_POST['oui']) AND empty($_POST['non']))
         {
             try
             {
-            $bdd = new PDO('mysql:host=localhost; dbname=hbo-bdd', 'root', '');
+            $bdd = new PDO('mysql:host=localhost; dbname=bdd_hbo', 'root', '');
             }
             catch (Exception $e)
             {
             die('Erreur : ' . $e->getMessage());
             }
-
-              $sup_utilisateur=$bdd->prepare('DELETE * FROM utilisateur WHERE pseudo=? ');
-              $sup_utilisateur->execute($_SESSION['pseudo']);
-              echo'Votre mdp et pseudo on été supprimés';
-              exit;
-
+              $pseudo_supprimé=$_SESSION['pseudo'];
+              $sup_utilisateur=$bdd->prepare('DELETE FROM utilisateur WHERE pseudo=? ')  or die(print_r($bdd->errorInfo()));
+              $sup_utilisateur->execute(array($pseudo_supprimé)) or die(print_r($bdd->errorInfo()));
+              $delai=2;
+              $url='http://localhost/home_be_one/tests/accueil_connecte_okok.php';
+              echo 'Votre mdp et pseudo ont été supprimé';
+              header("Refresh: $delai;url=$url");
         }
-    else {
-      $delai=1;
+    else
+      {
+      $delai=2;
       $url='http://localhost/home_be_one/tests/accueil_connecte_okok.php';
       echo 'Vous êtes redirigé(é) vers votre page d\'accueil ';
       header("Refresh: $delai;url=$url");
